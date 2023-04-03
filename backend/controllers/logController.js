fc0b18337d8843cabdd41e6623a06f6f96a3b300
goophygamer <yash.k14603@hotmail.com>
2023-04-03 19:16:11 +0530

    crud successful

--- backend/controllers/logController.js
+++ backend/controllers/logController.js
@@ -1,7 +1,9 @@
 const asyncHandler = require('express-async-handler')
+const Logs = require('../model/logsModel')
 
 const getLogs =asyncHandler(async (req,res)=>{
-    res.json({message:'get logs '})
+    const logs = await Logs.find()
+    res.json({logs})
 })
 
 const setLogs = asyncHandler(async (req,res)=>{
@@ -9,15 +11,33 @@ const setLogs = asyncHandler(async (req,res)=>{
         res.status(400)
         throw new Error('Please add a text filed ')
     }
-    res.json({message:'set logs '})
+    const logs = await Logs.create({
+        text:req.body.text
+    })
+    res.json(logs)
 })
 
 const updateLogs =asyncHandler( async (req,res)=>{
-    res.json({message:'updaet logs '})
+   const logs = await Logs.findById(req.params.id)
+   if(!logs){
+    res.status(400)
+    throw new Error('no such log found ')
+   }
+   const updatedLog= await Logs.findByIdAndUpdate(req.params.id,req.body,{
+    new:true,
+})
+    res.json(updatedLog)
 })
 
 const deleteLogs = asyncHandler(async (req,res)=>{
-    res.json({message:'delete logs '})
+    const logs = await Logs.findById(req.params.id)
+    if(!logs){
+     res.status(400)
+     throw new Error('no such log found ')
+    }
+    await logs.remove()
+
+    res.json({id:req.params.id})
 })
 
 module.exports = {
